2f04e053244d9e28a3e6637558da9c91bdf7c26c
Shira Stein <sstein@jkha.org>
2025-04-08 17:41:37 -0400

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Shira Stein on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
