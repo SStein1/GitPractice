adeb900fbe81a3f5462415451abee914aa37694e
Shira Stein <sstein@jkha.org>
2025-04-08 18:33:03 -0400

    Add change 3

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text ("Goodbye, Shira!")
             Text ("Change 1")
             Text ("Change 2")
+            Text ("Change 3")
         }
         .padding()
     }
